0c2e05a2799a479eecc7cb9e7cdf6cc5eb08086b
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-11-10 22:36:31 -0500

    struggling with returning contract data

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-const wikiRouter = require('./controllers/wiki')
+const contractRouter = require('./controllers/contract')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/wiki', wikiRouter)
+app.use('/api/contract', contractRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ services/contract.js
@@ -0,0 +1,59 @@
+const amqp = require('amqplib/callback_api')
+require('dotenv').config()
+
+amqp.connect(process.env.CLOUDAMQP_URL, (error0, connection) => {
+        if (error0) {
+            console.log(error0)
+        }
+    
+        connection.createChannel((error1, channel) => {
+            if (error1) {
+                console.log(error1)
+            }
+            const queue = 'nft'
+    
+            channel.assertQueue(queue, {
+                durable: false
+            }, (error2) => {        // callback after assert
+                if (error2) {
+                    console.log(error2)
+                }
+    
+                const correlationId = generateUUid()
+                const send_msg = {
+                    "address": '0xF8C08433DD41eAeE2e424C9E91467aB27772d9ec'
+                }
+    
+                console.log('requesting')
+    
+                channel.consume(queue, (msg) => {
+                    if (msg.properties.correlationId == correlationId) {
+                        // got contract
+                        const msg_content = JSON.parse(JSON.parse(msg.content).contract)
+                        console.log(msg_content);
+
+                        setTimeout((msg_content) => {
+                            connection.close()
+                            process.exit(0)
+                        }, 500)
+                    }
+                }, {
+                    noAck: true
+                })
+    
+                channel.sendToQueue(queue,
+                    Buffer.from(JSON.stringify(send_msg)), {
+                        contentType: 'application/json',
+                        correlationId: correlationId,
+                        replyTo: queue
+                    })
+            })
+        })
+    })
+
+
+const generateUUid = () => {
+    return Math.random().toString() +
+        Math.random().toString() +
+        Math.random().toString()
+}
